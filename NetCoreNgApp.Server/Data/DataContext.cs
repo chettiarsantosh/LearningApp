226b696c6b95c26b25646c46e658b183046ee48f
Santosh Chettiar <chettiarsantosh@live.com>
2021-01-16 15:26:47 +0530

    End of section 2

---
+++ NetCoreNgApp.Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreNgApp.Server.Entities;
+
+namespace NetCoreNgApp.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
